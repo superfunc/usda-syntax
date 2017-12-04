4388c8410e1f090473ed104cc79017519fb8f3a5
superfunc <superfunc@protonmail.ch>
2017-12-03 16:57:20 -0800

    Upload more recent emacs file

--- emacs/usda-syntax.el
+++ emacs/usda-syntax.el
@@ -1,6 +1,3 @@
-;; See LICSENSE for details
-;; github.com/superfunc/usda-syntax
-
 (setq usda-font-lock-keywords                                                   
       (let* ((usda-keywords '("def" "class" "over" "payload" "subLayers"        
                               "references" "inherits" "variants" 
@@ -21,16 +18,29 @@
                            "matrix2d" "matrix3d" "matrix4d"                     
                            "quatd" "quatf" "quath"
                            "quatd[]" "quatf[]" "quath[]"
-                           "token" "string" "asset" "frame4d"))                  
+                           "token" "string" "asset" "frame4d"
+			   "add" "remove" "reorder"))
+	     (usda-special-names '("kind" "defaultPrim" "upAxis"
+			           "startTimeCode" "endTimeCode"
+			           "instanceable" "hidden" "active"))
                                                                                 
              (usda-keywords-regexp (regexp-opt usda-keywords 'words))           
              (usda-types-regexp (regexp-opt usda-types 'words))
-             (usda-dict-keys ()))
+	     (usda-assetp-regexp "\@")
+	     (usda-path-open-regexp "\<\/")
+	     (usda-path-close-regexp "\>")
+	     (usda-special-regexp (regexp-opt usda-special-names 'words))
+	     (usda-num-regexp (regexp-opt '("0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "."))))
                                                                                 
         `((,usda-types-regexp . font-lock-type-face)                            
-          (,usda-keywords-regexp . font-lock-keyword-face))))
-                                                                                
-(define-derived-mode usda-mode c-mode "usda mode"                               
+          (,usda-keywords-regexp . font-lock-keyword-face)
+	  (,usda-special-regexp . font-lock-builtin-face)
+	  (,usda-num-regexp . font-lock-constant-face)
+	  (,usda-path-open-regexp . font-lock-constant-face)
+	  (,usda-path-close-regexp . font-lock-constant-face)
+	  (,usda-assetp-regexp . font-lock-constant-face))))          
+                               
+(define-derived-mode usda-mode c-mode "usda mode"                     
   "Major mode for editing USD's ascii representation"
   (setq font-lock-defaults '((usda-font-lock-keywords))))                       
                                                                                 
